c2e2387fd696767302df2af041b07a05bf395844
SherryMaster <sherrymaster2@gmail.com>
2025-03-10 14:54:10 +0500

    add database logging

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,9 +7,9 @@ export const connentDB = async () => {
     }
     
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB connected ${conn.connection.host}`);
+    console.log(`MongoDB connected to ${conn.connection.host}`);
   } catch (error) {
-    console.log(`Error: ${error.message}`);
+    console.error(`MongoDB connection error: ${error.message}`);
     process.exit(1);
   }
 };
